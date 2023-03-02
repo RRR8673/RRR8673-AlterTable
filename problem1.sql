1d27a73a988be4818dbda7fc6b44976ae65803fe
Rafael Renigado <renigado@aol.com>
2023-03-02 22:39:47 +0000

    done

--- problem1.sql
+++ problem1.sql
@@ -1,4 +1,2 @@
--- ALTER TABLE table_name ADD column_name data_type [constraint]
--- site_user
--- write a SQL statement that adds a "lastname" column to the site_user table, of type varchar(100)
+
 ALTER TABLE site_user ADD lastname VARCHAR(100);
